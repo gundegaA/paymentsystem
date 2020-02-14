52c47caab05c1ad98cbc8af4b218df6a9a1d8268
gundegaA <gundega.bambe@gmail.com>
2020-02-14 19:59:18 +0200

    first commit

---
+++ PaySystem/src/lv/gundega/paysystem/Payee.java
@@ -0,0 +1,10 @@
+package lv.gundega.paysystem;
+
+public interface Payee {
+
+	String name();
+	Double grossPayment();
+	Integer bankAccount();
+	
+	
+}
---
+++ PaySystem/src/lv/gundega/paysystem/PaymentSystem.java
@@ -0,0 +1,29 @@
+package lv.gundega.paysystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaymentSystem {
+	
+	//izmantojam interface,kas ir abstrakta klase
+	private List <Payee> payees;
+	
+	public PaymentSystem(){
+		payees = new ArrayList<>();	
+	}
+	
+	public void addPayee(Payee payee){
+		if(!payees.contains(payee)){
+			payees.add(payee);
+		}
+	}
+	public void processPayments(){
+		for(Payee payee : payees){
+			Double grossPayment = payee.grossPayment();
+			
+			System.out.println("Paying to "+ payee.name());
+			System.out.println("Grosst "+grossPayment);
+			System.out.println("Transfered to Account: "+payee.bankAccount() );
+		}
+	}
+}
